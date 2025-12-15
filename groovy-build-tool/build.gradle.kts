bf3258bb6e48c2a9966fe4be53c2e342fc0d04ea
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2025-12-14 22:50:46 -0800

    refactor: use version catalog in groovy-build-tool

--- groovy-build-tool/build.gradle.kts
+++ groovy-build-tool/build.gradle.kts
@@ -1,28 +1,29 @@
 plugins {
-    kotlin("jvm")
+    alias(libs.plugins.kotlin.jvm)
 }
 
 dependencies {
+    // Kotlin
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(libs.kotlin.stdlib)
 
     // Logging
-    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation(libs.slf4j.api)
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation(libs.kotlin.coroutines.core)
 
     // Gradle Tooling API
-    implementation("org.gradle:gradle-tooling-api:8.4")
+    implementation(libs.gradle.tooling.api)
 
     // Testing
-    testImplementation(kotlin("test"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
-    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.kotlin.coroutines.test)
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.mockk)
 
     // Detekt Formatting
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.8")
+    detektPlugins(libs.detekt.formatting)
 }
 
 kotlin {
