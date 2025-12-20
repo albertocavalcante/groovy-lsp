0a472e41f95d0c2c214e5cd22f5d47feca0499fd
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2025-12-20 03:35:48 -0800

    feat(infra): add runner image selection and fix region validation
    - Add runner_image variable with friendly names (ubuntu-22, debian-12, etc.)
    - Create locals.tf with image name mapping to cloud identifiers
    - Update mgc_region validation (remove deprecated br-mgl1, br-mc1)
    - Set default region to br-ne1 (Northeast Brazil)
    - Pass image to magalu-github-runner module
    - Add image info to runner_config output

--- infra/runner/main.tf
+++ infra/runner/main.tf
@@ -3,16 +3,17 @@
 
 # To update the module ref to the latest commit from main:
 # NEW_REF=$(gh api /repos/albertocavalcante/magalu-github-runner/commits/main --jq .sha) && \
-# sed -i '' "s/ref=47708af56676a76c24823a4644f91db3f91e9421[a-f0-9]*/ref=${NEW_REF}/" infra/runner/main.tf
+# sed -i '' "s/ref=3b96e3efe3e1334924425b17d8cdf79a8edb7aed[a-f0-9]*/ref=${NEW_REF}/" infra/runner/main.tf
 
 module "runner" {
-  source = "github.com/albertocavalcante/magalu-github-runner?ref=47708af56676a76c24823a4644f91db3f91e9421"
+  source = "github.com/albertocavalcante/magalu-github-runner?ref=3b96e3efe3e1334924425b17d8cdf79a8edb7aed"
 
   runner_count                 = var.runner_count
   runner_name_prefix           = var.runner_name_prefix
   github_repository_url        = var.github_repo_url
   github_personal_access_token = var.github_token
   machine_type                 = var.machine_type
+  image                        = local.resolved_image
 
   # Labels for targeting from CI workflows
   # NOTE: "self-hosted" is auto-added by GitHub and does not need to be specified here.
